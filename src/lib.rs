498d124daf0a9a0ccbf8b89969b125ed9da55629
Fabian Vilers <fabian.vilers@dev-one.com>
2023-10-16 18:56:20 +0200

    Add the md5 implementation setup build toolchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn md5(value: &str) -> String {
+    let digest = md5::compute(value);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    format!("{:x}", digest)
 }
